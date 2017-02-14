8881d470529657f5af9cb3e4057411827fa01443
carlcheng <278959724@qq.com>
2017-02-14 17:38:44 +0800

    a tweak for more

--- BroadBlog/app/Http/Controllers/HomeController.php
+++ BroadBlog/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -27,6 +28,11 @@ class HomeController extends Controller
     }
     public function index()
     {
-        return view('homeGeneral');
+        $user = Auth::user();
+        if($user->name == 'admin'){
+            return view('home');
+        }else{
+            return view('homeGeneral');
+        }
     }
 }
